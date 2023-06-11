8cca9e0edae776f83697e52181ad58cfe227304d
Samir <samir_zulfiqar@yahoo.com>
2023-06-11 18:10:05 -0400

    created function to play the  first round

--- script.js
+++ script.js
@@ -15,5 +15,29 @@ let playerSelection = promptAnswer.toLowerCase()
 
 console.log(playerSelection)
 
+function singleRound(playerSelection, computerSelecion) {
+    if (playerSelection === "rock" && computerSelecion === "Paper") {
+        return "You Lose! Paper beats Rock"
+    } else if (playerSelection === "rock" && computerSelecion === "Scissors") {
+        return "You Win! Rock beats Scissors"
+    } else if (playerSelection === "rock" && computerSelecion === "Rock") {
+        return "It's a Draw!"
+    } else if (playerSelection === "paper" && computerSelecion === "Paper") {
+        return "It's a Draw"
+    } else if (playerSelection === "paper" && computerSelecion === "Scissors") {
+        return "You Lose! Scissors beats Paper"
+    } else if (playerSelection === "paper" && computerSelecion === "Rock") {
+        return "You Win! Paper beats Rock"
+    } else if (playerSelection === "scissors" && computerSelecion === "Paper") {
+        return "You Win! Scissors beats Paper"
+    } else if (playerSelection === "scissors" && computerSelecion === "Scissors") {
+        return "It's a Draw" 
+    } else if (playerSelection === "scissors" && computerSelecion === "Rock") {
+        return "You Lose! Rock beats scissors"
+    } else return "please write correctly"
+}
 
+let computerSelecion = getComputerChoice() 
+console.log(computerSelecion)
+console.log(singleRound(playerSelection, computerSelecion))
 
