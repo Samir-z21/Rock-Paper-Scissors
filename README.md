9576dd7d4ab37cc6d6ae94be2bbda3b51420eec3
Samir <samir_zulfiqar@yahoo.com>
2023-06-12 14:22:33 -0400

    added more meaning to the console.log replies and added to the algorithm text

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 //Peusocode//
 
     1.
-    Create a function called getComputerChoice,
-    Which randomly returns values such as 'Rock', 'Paper' or 'Scissors',
+    Create a function that randomly returns values 'Rock', 'Paper' or 'Scissors',
         a. create a variable that contains an array of these values,
         b. create a code that generates either number 0, 1, or 2 at random,
-        c. assign the array to the random generated number so that it picks from it,
+        c. assign the array to it so that it picks the value that is in that position,
         d. return the randomly selected value,
     
     2.
     Create a function that plays a single round of 'Rock Paper Scissors',
+        a. create if else statements comparing user's input to the cpu's selection,
+        b. return the result of the round, 
+    
+    3.
+    Create a function that plays a game of 5 rounds keeping count of the score and stating the winner at the end of it, 
+        a. call upon previous function 5 times,
+        b. show and update the score after each round,
+        c. end the game after 5 rounds and declare a winner, 
--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 // Function that picks a random value from the array and returns it //
     function getComputerChoice() {
-
-    // Array containing desired values //
     let selections = ["Rock", "Paper", "Scissors"]
-
-    // Genereting a value from the array at random and returning it //
     let computerSelecion = selections[Math.floor(Math.random()* selections.length)];
     return computerSelecion
 }
@@ -13,27 +9,39 @@
 let promptAnswer = prompt ("Rock, Paper or Scissors? ")
 let playerSelection = (promptAnswer.slice(0,1).toUpperCase() + promptAnswer.slice(1).toLowerCase())
 
-console.log(playerSelection)
-
-// Function for a single round of play // 
+// Function for a single round of play //
 function singleRound(playerSelection, computerSelecion) { 
     if (playerSelection === computerSelecion) {
-        return "It's a Draw!" 
+        return ("It's a Draw!") 
     } else if (
         (playerSelection === "Rock" && computerSelecion === "Paper") || (playerSelection === "Paper" && computerSelecion === "Scissors") || (playerSelection === "Scissors" && computerSelecion === "Rock")
         ){
-        return "You Lose! " + computerSelecion + " beats " + playerSelection
+        return ("You Lose! " + computerSelecion + " beats " + playerSelection)
     } else if ( 
         (playerSelection === "Rock" && computerSelecion === "Scissors") ||(playerSelection === "Paper" && computerSelecion === "Rock") ||
         (playerSelection === "Scissors" && computerSelecion === "Paper")
         ){
-        return "You Win! " + playerSelection + " beats " + Scissors
+        return ("You Win! " + playerSelection + " beats " + computerSelecion)
     }  else { 
-        return "please write correctly"
+        return "please type correctly idiot"
     }
 }
 
 let computerSelecion = getComputerChoice() 
-console.log(computerSelecion)
+// To add meaningful replies //
+let playerMessage = "You selected " + playerSelection + "." ;
+let cpuMessage = "The CPU selected " + computerSelecion + ".";
+
+
+
+
+
+
+
+// Console checks //
+console.log(playerMessage)
+console.log(cpuMessage)
 console.log(singleRound(playerSelection, computerSelecion))
+
+
 
